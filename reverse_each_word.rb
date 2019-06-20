066d4f04bf33f197190fdc4f0630930fb0c6179a
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-20 01:52:11 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+
+=begin
+def reverse_each_word(strings)
+   my_array = []
+    strings.split do |string|
+      my_array << string.reverse
+    end
+    my_array.join(" ")
+  end 
+=end
+    
+  def reverse_each_word(strings)
+    strings.split.collect do |string|
+      string
+    end
+    [reverse_each_word(strings)].join(" ")
+  end 
+  
